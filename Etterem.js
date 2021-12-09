db23244b48d8f44864e8e282ecdf378d0a54a62b
ho-ho-h <hollopisti550@gmail.com>
2021-12-09 10:20:22 +0100

    frontend

--- Etterem.js
+++ Etterem.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, FlatList } from 'react-native';
 
 export default class Etterem extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {
+
+      dataSource:[]
+    };
+
+  }
+
+  componentDidMount(){
+    return fetch('http://localhost:3000/etterem')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+        
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
   }
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
+      <View style={{padding: 10,}}>
+       <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => 
+          <View style={{borderWidth:1,borderRadius:20,marginLeft:20,marginRight:20,padding:10,paddingLeft:15,}}>
+            <Text style={{fontSize:20,padding:3}}>{item.etterem_nev} </Text>
+            <Text style={{fontSize:15,padding:3}}>{item.etterem_lakcim} </Text>
+            <Text style={{fontSize:15,padding:3}}>{item.etterem_telefon} </Text>
+            <Text style={{fontSize:15,padding:3}}>{item.etterem_nyitas} </Text>
+          </View>
+        }
+          keyExtractor={({etterem_id}, index) => etterem_id}
         />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+
       </View>
     );
   }
