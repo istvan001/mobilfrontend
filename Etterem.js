859a66a87bbc5fe4f19efb0fb5b38cd5598a8eac
ho-ho-h <hollopisti550@gmail.com>
2021-12-09 13:46:01 +0100

    frontend

--- Etterem.js
+++ Etterem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, FlatList } from 'react-native';
+import { Text, TextInput, View, FlatList,Image } from 'react-native';
 
 export default class Etterem extends Component {
   constructor(props) {
@@ -12,7 +12,7 @@ export default class Etterem extends Component {
   }
 
   componentDidMount(){
-    return fetch('http://localhost:3000/etterem')
+    fetch('http://localhost:3000/etterem' )
       .then((response) => response.json())
       .then((responseJson) => {
 
@@ -23,7 +23,7 @@ export default class Etterem extends Component {
 
         });
 
-        alert(JSON.stringify(this.state.dataSource))
+        //alert(JSON.stringify(this.state.dataSource))
 
       })
       .catch((error) =>{
@@ -32,12 +32,14 @@ export default class Etterem extends Component {
   }
 
   render() {
+    
     return (
       <View style={{padding: 10}}>
        <FlatList
           data={this.state.dataSource}
           renderItem={({item}) => 
-          <View style={{borderWidth:1,borderRadius:20,marginLeft:20,marginRight:20,padding:10,paddingLeft:15,}}>
+          <View style={{borderWidth:1,borderRadius:20,marginLeft:20,marginRight:20,marginBottom:5,padding:10,paddingLeft:15,}}>
+            <Image  source={{uri: 'http://localhost:3000/'+item.etterem_kep}} style={{width:300,height:300,marginLeft:"auto",marginRight:"auto"}} /> 
             <Text style={{fontSize:20,padding:3}}>{item.etterem_nev} </Text>
             <Text style={{fontSize:15,padding:3}}>{item.etterem_lakcim} </Text>
             <Text style={{fontSize:15,padding:3}}>{item.etterem_telefon} </Text>
